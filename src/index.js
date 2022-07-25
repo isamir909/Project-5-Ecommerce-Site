cbd2e6de414df160de36a12f2f24913d4c403278
isamir909 <samirlohiya909@gmail.com>
2022-07-25 12:46:25 +0530

    added index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const bodyParser = require('body-parser');
+const router = require('./router/route');
+const app= express();  
+const mongoose = require('mongoose');
+const multer=require('multer');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer().any())
+
+mongoose.connect("mongodb+srv://samirlohiya909:Lohiya123@samirlohiya.nszppy8.mongodb.net/ProductsManagementGroup2?retryWrites=true&w=majority",{
+    useNewUrlParser:true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/',router)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
---
+++ src/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userModel= new mongoose.Schema({
+    fname: {type:String, required:true, trim:true},
+    
+    lname: {type:String, required:true, trim:true},
+  
+    email: {type:String, required:true, trim:true, unique:true, lowercase:true},
+
+    profileImage:{type:String, required:true, trim:true},
+ 
+    phone:  {type:String, required:true, trim:true, unique:true},
+  
+    password:  {type:String, required:true,minLength:8, maxLength:15},
+
+    address: { 
+        shipping: {
+          street:  {type:String, required:true, trim:true},
+          city:    {type:String, required:true, trim:true},
+          pincode: {type:Number, required:true, trim:true},
+        },
+        billing: {
+            street: {type:String, required:true, trim:true},
+            city:   {type:String, required:true, trim:true},
+            pincode:{type:Number, required:true, trim:true}
+          }
+    }
+},{timestamps:true})
+
+module.exports =mongoose.model('user',userModel)
