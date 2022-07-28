609bec737bb99d4f7c36435d22149e5bbab7ee79
isamir909 <samirlohiya909@gmail.com>
2022-07-28 12:45:43 +0530

    added validation on product

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -72,9 +72,9 @@ const createProduct = async function (req, res) {
                     status: false,
                     message: `This size: '${reqSize[i].trim()}' is not available`,
                 });
-                data.availableSizes=reqSize.map(function(x){return x.toUpperCase(); })
+                data.availableSizes=reqSize.map(function(x){return x.toUpperCase()});
         }
-        //-----Shipping
+        //-----Shipping 
         data.isFreeShipping = isFreeShipping.trim();
         if (!(isFreeShipping.trim() == "true" || isFreeShipping.trim() == "false"))
             return res.status(400).send({
@@ -162,5 +162,114 @@ const getProducts = async function (req, res) {
         res.send(error.message);
     }
 };
+const updateProduct = async function (req, res) {
 
-module.exports = { createProduct, getProducts };
+    try {
+
+        let data = req.body;
+        const productId = req.params.productId;
+        const files = req.files
+//enter id
+//valid id
+        let productData = await productModel.findOne({ _id: productId }).select({ _id: 0, updatedAt: 0, createdAt: 0, __v: 0 }).lean();
+        if (!productData) return res.status(404).send({ status: false, msg: "ProductId is not Correct" })
+
+        
+    if(data.title){
+    let findTitle = await productModel.findOne({ title: data.title });
+        if (findTitle) return res.status(400).send({ status: false, msg: "This Title is already exist" });
+    //null
+    }
+         productData.title = data.title
+        productData.description = data.description;//null
+
+        if(data.price){if(!validateNumber(data.price))return res.status(400).send({status:false,msg:'Price Must be in Integer'})}
+        productData.price = data.price;
+//if
+        // if(!(data.isFreeShipping=="true" || data.isFreeShipping=="false")){return res.status(400).send({status:false,msg:"Input must be in True or False"})}
+        // productData.isFreeShipping = data.isFreeShipping;
+//lowercase
+
+    if(data.style){
+    productData.style = data.style;
+    }
+
+     
+        var size = data.availableSizes.toUpperCase().split(",")
+        for (let i = 0; i < size.length; i++) {
+            if (!["S", "XS", "M", "X", "L", "XXL", "XL"].includes(size[i])) {
+                return res.status(400).send({ status: false, message: "Size Must Contain S, XS, M, X, L, XXL, XL" });
+                }
+              //case sensitive use map fun
+            //     else if(["S", "XS", "M", "X", "L", "XXL", "XL"].includes(size[i])){
+            //         return res.status(400).send({ status: false, message: "This Size Already Exist" }) 
+            // }
+            
+            productData.availableSizes = data.availableSizes.split(",")
+        }
+
+        
+        productData.installments = data.installments;
+
+        if (files) if (files.length != 0) {
+            if (files && files.length > 0) {
+                let uploadedFileURL = await uploadFile(files[0])
+                productData.productImage = uploadedFileURL
+            }
+            else { return res.status(400).send({ msg: "Enter The Product image" }) }
+        }
+//image valid formate
+
+        let updatedData = await productModel.findOneAndUpdate({ _id: productId }, { $set: productData }, { new: true });
+
+        return res.status(200).send({ status: true, message: updatedData })
+
+    } catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+
+
+
+}
+const getProductById = async function (req, res) {
+    try{
+      const productId = req.params.productId.trim()
+  
+      if(productId.length==0 ||productId==':productId')return res.status(400).send({status:false, message: "Enter user id" })
+      if(!isValidObjectId(productId))return res.status(400).send({status:false, message: "Invalid product id" })
+  
+      const product = await productModel.findOne({_id:productId, isDeleted : false})
+      if(product== null){return res.status(404).send({status:false, msg:"product not found"})}
+      else{//response structure
+          return res.status(200).send({status:true, msg:"Product Data",data:product})
+      }
+    }  
+    catch (error) {
+      
+      return res.status(500).send({status:false, message:error.message});
+  }
+      
+  }
+
+  const deleteProductById = async function (req, res) {
+    try{//response structure
+        const productId = req.params.productId.trim()
+        
+    if(productId.length==0 ||productId==':productId')return res.status(400).send({status:false, message: "Enter user id" })
+    if(!isValidObjectId(productId))return res.status(400).send({status:false, message: "Invalid product id" })
+        
+        const isProductdelete = await productModel.findOne({_id:productId,isDeleted:true})
+        if(isProductdelete){return res.status(404).send({status:false,msg:"product not found"})}
+        
+        const productDelete = await productModel.findOneAndUpdate({_id:productId,isDeleted:false},{isDeleted:true, deletedAt: Date.now()})
+        return res.status(200).send({status:true,msg:"product deleted successfully"})
+    }
+    catch(error) {
+        return res.status(500).send({status:false,message:error.message})
+    }
+}
+
+
+
+
+module.exports = { createProduct, getProducts,updateProduct ,deleteProductById,getProductById};
--- src/router/route.js
+++ src/router/route.js
@@ -4,17 +4,19 @@ const userController=require('../controller/userController')
 const productController=require('../controller/productController')
 const {authenticate}=require('../middleware/authentication')
 
-
+//......................User Feature...........................//
 router.post('/register',userController.createUser)
 router.post('/login',userController.login)
-
 router.get('/user/:userId/profile',authenticate,userController.getUserProfile)
 router.put('/user/:userId/profile',authenticate,userController.updateData)
-router.get('/products',productController.getProducts)
 
+
+//......................Product Feature...........................//
 router.post('/products',productController.createProduct)
-
-
+router.get('/products',productController.getProducts)
+router.get('/products/:productId',productController.getProductById)
+router.put('/products/:productId',productController.updateProduct)
+router.delete('/products/:productId',productController.deleteProductById)
 
 
 
